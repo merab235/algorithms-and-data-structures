fee3d5a16bdf724ca9f260ffe7a2ec58b41e1a83
merab235 <issakamallia@gmail.com>
2024-12-09 19:30:29 +0300

    general

---
+++ run_all_lab_files.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+lab_tasks = [1,2]
+
+for i in lab_tasks:
+    os.chdir(f'lab{i}/')
+
+    print(f'\033[34mRUNNING LAB #{i}\033[0m')
+    print()
+    path_test = f'run_files_lab{i}.py'
+    result = subprocess.run(['python',path_test], capture_output=True, text=True).stdout
+    print(result)
+    os.chdir(f'..')
